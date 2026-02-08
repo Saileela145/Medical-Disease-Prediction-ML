93abc2fbc8546e821b1123a0a546804597e738b3
SAILEELA D <156500159+Saileela145@users.noreply.github.com>
2026-02-09 01:06:56 +0530

    Add script to train Logistic Regression model

---
+++ src/train_model.py
@@ -0,0 +1,20 @@
+# train_model.py
+# This script trains a Logistic Regression model for disease prediction
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+data = pd.read_csv("../dataset/diabetes.csv")
+
+X = data.drop("Outcome", axis=1)
+y = data["Outcome"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+print("Model training completed")
