cf2eceefc85c42c5bd0976e208460036b7138473
SAILEELA D <156500159+Saileela145@users.noreply.github.com>
2026-02-11 09:41:07 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -14,7 +14,8 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = LogisticRegression()
+model = LogisticRegression(max_iter=1000)
+
 model.fit(X_train, y_train)
 with open("model.pkl", "wb") as file:
     pickle.dump(model, file)
