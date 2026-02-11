3c257ce7b13f2ed8732f88167ab6453e38d84a36
SAILEELA D <156500159+Saileela145@users.noreply.github.com>
2026-02-11 08:12:02 +0530

    Update and rename src/train_model.py to train_model.py

--- train_model.py
+++ train_model.py
@@ -1,6 +1,6 @@
 # train_model.py
 # This script trains a Logistic Regression model for disease prediction
-
+import pickle
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -16,5 +16,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 model = LogisticRegression()
 model.fit(X_train, y_train)
+with open("model.pkl", "wb") as file:
+    pickle.dump(model, file)
 
 print("Model training completed")
